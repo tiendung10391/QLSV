2c65b212da5e1db317a069893e19cd66e4378716
Nguyễn Tiến Dũng <tiendung10391@gmail.com>
2015-08-27 20:44:02 +0700

    webservices qlsv
    webservices qlsv

---
+++ ServicesQLSV/src/main/java/com/itplus/servicesqlsv/Model/Logger.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itplus.servicesqlsv.Model;
+
+/**
+ *
+ * @author Dung NT
+ */
+public class Logger {
+
+    private String classname;
+
+    public Logger(String classname) {
+        this.classname = classname;
+    }
+
+    public void log(String msg) {
+        System.out.println("[" + this.classname + "] " + msg);
+    }
+
+    public void error(String msg) {
+        System.err.println("[ERROR] [" + this.classname + "] " + msg);
+    }
+}
---
+++ ServicesQLSV/src/main/java/com/itplus/servicesqlsv/rest/SwResource.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itplus.servicesqlsv.rest;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+
+/**
+ * REST Web Service
+ *
+ * @author Dung NT
+ */
+@Path("sw")
+public class SwResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of SwResource
+     */
+    public SwResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.itplus.servicesqlsv.rest.SwResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/json")
+    public String getJson() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of SwResource
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/json")
+    public void putJson(String content) {
+    }
+}
