3983b997aeb9db4f618816bdf04b8c33c1623e7d
Nguyễn Tiến Dũng <tiendung10391@gmail.com>
2015-09-05 17:37:53 +0700

    them chuc nang filter

---
+++ ProjectNhom1/src/java/itplus/project/util/MessageUtil.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itplus.project.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Dung NT
+ */
+public class MessageUtil {
+    public void addErrorMessage(String message) {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+    }
+
+    public void addErrorMessage(Exception ex) {
+        FacesContext.getCurrentInstance().addMessage(
+                null,
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, ex.getMessage(),
+                null));
+    }
+
+    public void addSuccessMessage(String message) {
+        FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, message, null));
+    }
+}
