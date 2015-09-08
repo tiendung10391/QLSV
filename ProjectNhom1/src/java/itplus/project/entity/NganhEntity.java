5605ca4e7206c2e5e4419de2ef1f52bbfcf9ae02
Nguyễn Tiến Dũng <tiendung10391@gmail.com>
2015-09-08 11:09:20 +0700

    update

---
+++ ProjectNhom1/src/java/itplus/project/entity/NganhEntity.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itplus.project.entity;
+
+/**
+ *
+ * @author kunph_000
+ */
+public class NganhEntity {
+    private String maNganh = "";
+    private String tenNganh = "";
+
+    public String getMaNganh() {
+        return maNganh;
+    }
+
+    public void setMaNganh(String maNganh) {
+        this.maNganh = maNganh;
+    }
+
+    public String getTenNganh() {
+        return tenNganh;
+    }
+
+    public void setTenNganh(String tenNganh) {
+        this.tenNganh = tenNganh;
+    }
+    @Override
+    public NganhEntity clone() throws CloneNotSupportedException {
+        return (NganhEntity) super.clone();
+    }
+}
---
+++ ProjectNhom1/src/java/itplus/project/entity/SinhVienEntity.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itplus.project.entity;
+
+/**
+ *
+ * @author kunph_000
+ */
+public class SinhVienEntity {
+
+    private String maSV = "";
+    private String tenSV = "";
+    private String ngaySinh = "";
+    private Boolean gioiTinh;
+    private String sdt;
+    private String diaChi = "";
+    private String queQuan = "";
+    private String email = "";
+    private String maLop = "";
+
+    public String getMaSV() {
+        return maSV;
+    }
+
+    public void setMaSV(String maSV) {
+        this.maSV = maSV;
+    }
+
+    public String getTenSV() {
+        return tenSV;
+    }
+
+    public void setTenSV(String tenSV) {
+        this.tenSV = tenSV;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public Boolean getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(Boolean gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getQueQuan() {
+        return queQuan;
+    }
+
+    public void setQueQuan(String queQuan) {
+        this.queQuan = queQuan;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMaLop() {
+        return maLop;
+    }
+
+    public void setMaLop(String maLop) {
+        this.maLop = maLop;
+    }
+
+    @Override
+    public SinhVienEntity clone() throws CloneNotSupportedException {
+        return (SinhVienEntity) super.clone();
+    }
+}
